363bd76f50c24eb6ddb9dc10ee0965d44d43876b
Jonathan D Borgia <jonathan.borgia@apprenticefs.com>
2024-09-19 14:40:06 -0600

    Update example.ts

--- example.ts
+++ example.ts
@@ -7,6 +7,7 @@ type Nested3 = {
   deeplyNested: boolean;
   deeplyNestedArray: number[];
   deeplyNestedUndefinedStringTyped: string;
+  deeplyNestedStringOrUndefined?: string | undefined | unknown;
   deeplyNestedUndefined?: string | unknown;
 };
 
@@ -39,16 +40,18 @@ const storeConfigurationAndInititalizationObject: MyType = {
       nested3: {
         deeplyNested: true,
         deeplyNestedArray: [4, 5],
-        deeplyNestedUndefinedStringTyped: undefined as unknown as string, // the initial state is undefined, but after it MUST be of type string
-        deeplyNestedUndefined: undefined as unknown | string // the initial state is undefined, and after it CAN be of type undefined or string
+        deeplyNestedUndefinedStringTyped: undefined as unknown as string, // STRONG TYPE(string only): the initial state is undefined, but after it MUST be of type string
+        deeplyNestedStringOrUndefined: null as unknown | string, // WEAKER TYPE(unknown | string): the initial state is undefined, and after it CAN be of type undefined or string
+        deeplyNestedUndefined: undefined // STRONG TYPE(undefined only): example only, you would likely never do this. the initial state is undefined, but after it MUST be of type string
       },
       nestedSignal4: signal({
+        // signal is here so the object will be left as a signal rather than wrapping each of its fields in signals.
         deeplyNested: true,
         deeplyNestedArray: [4, 5],
         deeplyNestedUndefinedStringTyped: undefined as unknown as string, // the initial state is undefined, but after it MUST be of type string
         deeplyNestedUndefined: undefined as unknown | string // the initial state is undefined, and after it CAN be of type undefined or string
       }),
-      nestedSignal5: signal([4, 5])
+      nestedSignal5: signal([4, 5]) // signal is here so the object will be left as a signal rather than wrapping each of its fields in signals.
     }
   }
 };
@@ -70,5 +73,8 @@ const value6: {
 } = store.prop2.nested2.nestedSignal4();
 const value7: number[] = store.prop2.nested2.nestedSignal5();
 
+const unwrapedNested3: Partial<Nested3> = store.prop2.nested2.nested3.unwrap();
+
 // Values can be modified in any way that WritableSignals can as they are just Signals
-store.prop2.nested1.mutate((val) => val + ' world');
+store.prop2.nested1.update((val) => val + ' world');
+store.prop2;
