38278ec429106decd8d966a4a86810b97ad99e1a
Borgia <jborgia@calportland.com>
2025-12-28 20:27:57 -0700

    chore(enterprise): add missing src/index.d.ts for declaration verification

---
+++ packages/enterprise/src/index.d.ts
@@ -0,0 +1,6 @@
+export * from './lib/diff-engine';
+export * from './lib/path-index';
+export * from './lib/update-engine';
+export * from './lib/enterprise-enhancer';
+export * from './lib/scheduler';
+export * from './lib/thread-pools';
